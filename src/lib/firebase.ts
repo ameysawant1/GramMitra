549458fe93f089097486160c5e0db71e14ea55aa
Amey Sawant <91938168+ameysawant1@users.noreply.github.com>
2025-04-15 09:55:18 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,7 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBwTqR9v4YlWv-OEyBhGIp69bXzh97c-co",
-  authDomain:"grammitra-a60ec.firebaseapp.com",
-  projectId:"grammitra-a60ec",
-  storageBucket: "grammitra-a60ec.firebasestorage.app",
-  messagingSenderId:"1017703922151",
-  appId:"1:1017703922151:web:c30ae9ae4c6f897674e542",
-}
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
